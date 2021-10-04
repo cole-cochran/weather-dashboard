db2632d7485f2c720a10943b85c84ee4c91eee7a
Cole Cochran <colecochran405@gmail.com>
2021-10-04 15:24:54 -0500

    connecting to API and pulling data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,24 @@
-var API_key = "327acbd7d79e6254e6714764cab1a033";
-var oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API_key}`;
-var callFiveDayAPI = `api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_key}`;
+var API_key = '327acbd7d79e6254e6714764cab1a033';
+var weatherBtn = document.getElementById("get-weather");
+weatherBtn.addEventListener("click",function(){
+    var city_name = document.getElementById("city_name").value
+    console.log(city_name);
+    getCoordinates(city_name)
+})
 
-var part ='hourly'
-var city_name = 'chicago'
-
-function getCoordinates(){
+function getCoordinates(city_name){
+    var callFiveDayAPI = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=${API_key}&units=imperial`;
     console.log(callFiveDayAPI);
     fetch(callFiveDayAPI)
     .then(function(response){
         if (response.ok){
             response.json()
             .then(function(data){
-                console.log(data.city.coord);
-                latitude = data.city.coord.lat;
-                logitude = data.city.coord.long;
+                console.log(data)
+                console.log(data.coord);
+                var latitude = data.coord.lat;
+                var longitude = data.coord.lon;
+                getWeatherOneDay(latitude,longitude)
                 }
             ) 
             } else {
@@ -23,17 +27,15 @@ function getCoordinates(){
     
     }) 
 }
-function getWeatherOneDay(oneCallAPI){
+function getWeatherOneDay(latitude,longitude){
+    var oneCallAPI = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${API_key}&units=imperial`;
     console.log(oneCallAPI);
     fetch(oneCallAPI)
     .then(function(response){
         if (response.ok){
             response.json()
             .then(function(data){
-                console.log(data.city.coord);
-                latitude = data.city.coord.lat;
-                logitude = data.city.coord.long;
-                getWeatherOneDay();
+                console.log(data);
                 }
             ) 
             } else {
@@ -41,6 +43,6 @@ function getWeatherOneDay(oneCallAPI){
             }
 })
 }
-getCoordinates();
+
 
 
